3d380d294074f09fd7cd224033adc3f09f4ea9ad
Nikhil <m.j882600@gmail.com>
2024-03-15 22:13:07 +0530

    same as previous commit but some minor changes

--- products/product-single.php
+++ products/product-single.php
@@ -29,9 +29,10 @@ if (isset($_GET['id'])) {
 		$price = $_POST['price'];
 		$quantity = $_POST['quantity'];
 		$size = $_POST['size'];
+		$product_id = $_POST['product-id'];
 		$user_id = $_SESSION['user_id'];
 
-		$query = "INSERT INTO cart (name, image, price, description, size, quantity, product_id, user_id) VALUES ('{$name}', '{$image}', '{$price}', '{$description}', '{$size}', {$quantity}, {$id}, {$user_id})";
+		$query = "INSERT INTO cart (name, image, price, description, size, quantity, product_id, user_id) VALUES ('{$name}', '{$image}', '{$price}', '{$description}', '{$size}', {$quantity}, {$product_id}, {$user_id})";
 		mysqli_query($conn, $query) or die("Query Unsuccessful");
 
 		echo "<script>alert('Added to cart successfully')</script>";
@@ -100,22 +101,11 @@ if (isset($_GET['id'])) {
 								</span>
 							</div>
 						</div>
-						<input type="text" name="name" value="<?php echo $product['name']; ?>">
-						<input type="text" name="image" value="<?php echo $product['image']; ?>">
-						<input type="text" name="description" value="<?php echo $product['description']; ?>">
-						<input type="text" name="price" value="<?php echo $product['price']; ?>">
-						<input type="text" name="product-id" value="<?php echo $id; ?>">
-						<!-- <?php
-								if (isset($_SESSION['user_id'])) {
-								?>
-							<button class="btn btn-primary py-3 px-4 cart" name="submit" type="submit">
-								Add to cart
-							</button>
-						<?php } else { ?>
-							<button disabled class="btn btn-primary py-3 px-4 cart" name="submit" type="submit">
-								Login to add to cart
-							</button>
-						<?php } ?> -->
+						<input hidden type="text" name="name" value="<?php echo $product['name']; ?>">
+						<input hidden type="text" name="image" value="<?php echo $product['image']; ?>">
+						<input hidden type="text" name="description" value="<?php echo $product['description']; ?>">
+						<input hidden type="text" name="price" value="<?php echo $product['price']; ?>">
+						<input hidden type="text" name="product-id" value="<?php echo $product['id']; ?>">
 						<?php
 						if (isset($_SESSION['user_id'])) {
 						?>
